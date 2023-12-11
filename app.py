d4ce18ad0221677641063e3915e8348fe84b4153
Eric Huie <erichuie97@gmail.com>
2023-12-11 15:08:41 -0800

    added homepage

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Flask app for Cupcakes"""
 import os
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from models import db, Cupcake, connect_db, DEFAULT_IMAGE_URL
 
 app = Flask(__name__)
@@ -12,6 +12,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.get("/")
+def homepage():
+    """Displays homepage"""
+    return render_template("index.html")
+
 
 @app.get("/api/cupcakes")
 def get_cupcakes_data():
@@ -25,6 +31,7 @@ def get_cupcakes_data():
     serialized = [cupcake.serialize() for cupcake in cupcakes_data]
     return jsonify(cupcakes=serialized)
 
+
 @app.get("/api/cupcakes/<int:cupcake_id>")
 def get_single_cupcake_data(cupcake_id):
     """
@@ -34,6 +41,7 @@ def get_single_cupcake_data(cupcake_id):
     cupcake_data = Cupcake.query.get_or_404(cupcake_id)
 
     return jsonify(cupcake=cupcake_data.serialize())
+
 
 @app.post("/api/cupcakes")
 def add_cupcake():
@@ -53,10 +61,12 @@ def add_cupcake():
 
     return (jsonify(cupcake=new_cupcake.serialize()), 201)
 
+
 @app.patch("/api/cupcakes/<int:cupcake_id>")
 def update_single_cupcake(cupcake_id):
     """
     Update existing cupcake.
+    Can send JSON {flavor, size, rating, image_url}, each field is optional.
     Returns JSON {cupcake: {id, flavor, size, rating, image_url}}"""
 
     cupcake = Cupcake.query.get_or_404(cupcake_id)
@@ -64,12 +74,17 @@ def update_single_cupcake(cupcake_id):
     cupcake.flavor = request.json.get("flavor", cupcake.flavor)
     cupcake.size = request.json.get("size", cupcake.size)
     cupcake.rating = request.json.get("rating", cupcake.rating)
-    cupcake.image_url = request.json.get("image_url", cupcake.image_url)
-    #TODO: Handle DEFAULT_IMAGE_URL (check for len?). Add what can be sent in request to docstring
+
+    image_url = request.json.get("image_url", cupcake.image_url).strip()
+    if not image_url:
+        cupcake.image_url = DEFAULT_IMAGE_URL
+    else:
+        cupcake.image_url = image_url
 
     db.session.commit()
 
     return jsonify(cupcake=cupcake.serialize())
+
 
 @app.delete("/api/cupcakes/<int:cupcake_id>")
 def delete_single_cupcake(cupcake_id):
