95bca996a378dabcb66a2d87ac92db4c4d29d238
Eric Huie <erichuie97@gmail.com>
2023-12-11 16:10:59 -0800

    added form and form submission

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -2,6 +2,10 @@
 
 const $cupcakeDisplay = $("#cupcake_data_list");
 const $cupcakeForm = $("#cupcake_data_form");
+const $flavorInput = $("#flavor_input");
+const $sizeInput = $("#size_input");
+const $ratingInput = $("#rating_input");
+const $imageInput = $("#image_input");
 
 
 // 1) Fetch cupcake list (Fetch API)
@@ -23,7 +27,34 @@ async function getCupcakeData(){
 
   for (let cupcake of cupcakes)
   {
-    $cupcakeDisplay.append($("<li>Cupcake</li>"));
+    $cupcakeDisplay.append($(`<li>
+    <p>Cupcake flavor: ${cupcake.flavor}</p>
+    <p>Cupcake rating: ${cupcake.rating}</p>
+    <p>Cupcake size: ${cupcake.size}</p>
+    <img src="${cupcake.image_url}">
+    </li>`));
   }
+}
+
+async function formSubmission(evt){
+  evt.preventDefault();
+
+  inputs = {
+    "flavor": $flavorInput.val(),
+    "size": $sizeInput.val(),
+    "rating": $ratingInput.val(),
+    "image_url": $imageInput.val(),
+  };
+
+  const response = await fetch("/api/cupcakes/", {
+    method: "POST",
+    body: JSON.stringify(inputs),
+    headers: {
+      "Content-Type": "applcation/json"
+    }
+  });
+
 
 }
+
+$cupcakeForm.on("submit", formSubmission);
